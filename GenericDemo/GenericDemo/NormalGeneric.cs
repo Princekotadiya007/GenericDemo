9f7ec6c1b9975dab4bca7f540a90fe3a6771d2b7
Princekotadiya007 <princuuu77@gmail.com>
2023-02-17 13:02:03 +0530

    [prince]:add Generic

--- GenericDemo/GenericDemo/NormalGeneric.cs
+++ GenericDemo/GenericDemo/NormalGeneric.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace GenericDemo
 {
-    internal class NormalGeneric
+    public class NormalGeneric
     {
+        public static void IntNum(int[] array)
+        {
+            foreach(var num in array)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        public static void IntNum(double[] array)
+        {
+            foreach(double num in array)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        public static void IntNum(char[] array)
+        {
+            foreach (char num in array)
+            {
+                Console.WriteLine(num);
+            }
+        }
     }
 }
--- GenericDemo/GenericDemo/Program.cs
+++ GenericDemo/GenericDemo/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace GenericDemo
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            int[] intArray = { 1, 2, 3, 4, 5, 6, 7 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
+            char[] charArray = { 'a', 'b', 'c', 'd', 'e' };
+            //Normal Generic   // We can call direct Class name 
+            NormalGeneric.IntNum(intArray);
+            NormalGeneric.IntNum(doubleArray);
+            NormalGeneric.IntNum(charArray);
+            Console.WriteLine("-------------------------");
+
         }
     }
 }
